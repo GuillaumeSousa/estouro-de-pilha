9ee80d921f964275f2dae080171d1b8b41a7d5fd
Mylene <Mylene@172.26.29.195>
2013-01-16 17:26:18 +0100

    My first commit

---
+++ grails-app/domain/fr/isima/estouroDePilha/User.groovy
@@ -0,0 +1,17 @@
+package fr.isima.estouroDePilha
+
+import java.util.Date;
+
+class User {
+
+	String pseudo
+	String login
+	String password
+	String website
+	String location
+	Date birth_date
+	Date register_date
+	int reputation
+    static constraints = {
+    }
+}
