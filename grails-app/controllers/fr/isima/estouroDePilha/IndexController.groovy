8ee19b19552c0968f07e844febedddf2c56da27b
guillaume <guillaume@PC-de-guillaume>
2013-03-02 09:49:54 +0100

    question sort in index

--- grails-app/controllers/fr/isima/estouroDePilha/IndexController.groovy
+++ grails-app/controllers/fr/isima/estouroDePilha/IndexController.groovy
@@ -5,6 +5,6 @@ import fr.isima.estouroDePilha.Question
 class IndexController {
 
 	def index = {
-		[questionList: Question.findAll(), questionNumber:Question.count(), tagList:Tag.findAll()] 
+		[questionList: Question.findAll().sort{it.postedDate}.reverse(), questionNumber:Question.count(), tagList:Tag.findAll()] 
 	}
 }
